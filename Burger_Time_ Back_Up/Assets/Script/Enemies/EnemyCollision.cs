86da6f04e9bc15beb47ad66b25a9e93b4ce7af5d
Mohamud <detectivemomo1@gmail.com>
2018-11-03 23:00:49 -0400

    add classes for milestone3
    - add class Timer
    - add class EnemyController
    - add class EnemyCollision
    - add class GameHudController
    - add class Spawn

---
+++ Burger_Time_ Back_Up/Assets/Script/Enemies/EnemyCollision.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour {
+
+	private bool m_IsTouchPepper = false;
+	private bool m_IsTouchBurgerSlice = false;
+	#region public fuctions
+	public bool GetTouchPepper
+	{
+		get{return m_IsTouchPepper;}
+	}
+	public bool GetTouchBurgerSlice
+	{
+		get{return m_IsTouchBurgerSlice;}
+	}
+	public void ResetTouchBurgerSlice()
+	{
+		m_IsTouchBurgerSlice = false;
+	}
+	public void ResetTouchPepper()
+	{
+		m_IsTouchPepper = false;
+	}
+	#endregion
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	void OnTriggerStay2D(Collider2D Other)
+	{
+		if(Other.tag == "BurgerPart")
+		{
+			m_IsTouchBurgerSlice = true;
+		}
+		if(Other.tag == "Pepper")
+		{
+			m_IsTouchPepper = true;
+		}
+	}
+
+
+
+}
